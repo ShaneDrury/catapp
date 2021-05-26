e9b2f18d3db589858c2173c700a9b9f59196be12
Shane Drury <shane.drury@freeagent.com>
2021-05-26 20:41:33 +0100

    Styling for upload page

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,20 @@ import UploadCat from "./UploadCat";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const App = () => (
-  <main className="container">
-    <Router>
-      <Switch>
-        <Route path="/upload">
-          <UploadCat />
-        </Route>
-        <Route path="/">
-          <Uploaded />
-        </Route>
-      </Switch>
-    </Router>
-  </main>
+  <div className="section">
+    <main>
+      <Router>
+        <Switch>
+          <Route path="/upload">
+            <UploadCat />
+          </Route>
+          <Route path="/">
+            <Uploaded />
+          </Route>
+        </Switch>
+      </Router>
+    </main>
+  </div>
 );
 
 export default App;
--- src/UploadCat.tsx
+++ src/UploadCat.tsx
@@ -2,25 +2,50 @@ import React from "react";
 import { apiFromKey } from "./catsApi";
 
 import { useHistory } from "react-router-dom";
+import { faUpload } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const api = apiFromKey();
 
 const UploadCat = () => {
   const history = useHistory();
+  const [loading, setLoading] = React.useState<boolean>(false);
+
   const handleInputChange = async (
     event: React.ChangeEvent<HTMLInputElement>
   ) => {
     const eventFiles = event.target.files;
     if (eventFiles) {
       const file = eventFiles[0];
+      setLoading(true);
       await api.newCat(file);
       history.push("/");
     }
   };
   return (
-    <div>
-      <input type="file" name="file" onChange={handleInputChange} />
-    </div>
+    <section className="hero">
+      <div className="hero-body">
+        <p className="title">Upload a cat</p>
+      </div>
+      <div className="file">
+        <label className="file-label">
+          <input
+            disabled={loading}
+            onChange={handleInputChange}
+            className="file-input"
+            type="file"
+            name="file"
+          />
+          <span className="file-cta">
+            <span className="file-icon">
+              <FontAwesomeIcon icon={faUpload} />
+            </span>
+            <span className="file-label">Choose a file</span>
+          </span>
+        </label>
+      </div>
+      {loading && "Uploading cat..."}
+    </section>
   );
 };
 
