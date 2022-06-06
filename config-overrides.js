8589b2c5d859078ac9c901486921af1b3d1e5d1e
Shane Drury <shane@shanes-mbp.home>
2022-06-06 18:50:14 +0100

    Update react-scripts

--- config-overrides.js
+++
@@ -1,9 +0,0 @@
-const rewireHtmlWebpackPlugin = require('react-app-rewire-html-webpack-plugin')
-const rewireStyledComponents = require("react-app-rewire-styled-components");
-
-module.exports = function override(config, env) {
-  const overrideConfig = { catsApiKey: process.env.CATS_API_KEY }
-  config = rewireHtmlWebpackPlugin(config, env, overrideConfig)
-  config = rewireStyledComponents(config, env);
-  return config
-}
