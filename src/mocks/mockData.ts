f1a7dfde75bfd71acc5c1194209f3a935d8fe4a7
Shane Drury <shane@shanes-mbp.home>
2022-06-28 22:31:26 +0100

    Add some more realistic mocks

--- src/mocks/mockData.ts
+++ src/mocks/mockData.ts
@@ -13,11 +13,32 @@ const {
 } = getMockHandlers("https://api.thecatapi.com/v1");
 
 export const mockData = [
-  mockAllImages(ok([])),
-  mockAllFavourites(ok([])),
+  mockAllImages(
+    ok([
+      { id: "1", url: "logo512.png" },
+      { id: "2", url: "logo512.png" },
+    ])
+  ),
+  mockAllFavourites(ok([{ id: "1", image_id: "1" }])),
   mockDeleteFavourite("favourite_id")(ok({})),
   mockPostFavourite(ok({})),
-  mockAllVotes(ok([], { "pagination-count": "0" })),
+  mockAllVotes(
+    ok(
+      [
+        { value: 1, image_id: "1" },
+        { value: 1, image_id: "1" },
+      ],
+      { "pagination-count": "4" }
+    ),
+    ok(
+      [
+        { value: 1, image_id: "1" },
+        { value: 1, image_id: "1" },
+        { value: 1, image_id: "2" },
+      ],
+      { "pagination-count": "4" }
+    )
+  ),
   mockVoteUp(ok({})),
   mockVoteDown(ok({})),
   mockUploadCat(ok({})),
